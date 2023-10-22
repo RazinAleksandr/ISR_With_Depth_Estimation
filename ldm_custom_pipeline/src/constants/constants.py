2ed422fbc501708598d4bc3325823cd77d32b10c
Aleksandr Razin <razin.x.aleks@gmail.com>
2023-10-22 20:15:07 +0300

    modified:   ldm_custom_pipeline/src/constants/constants.py 	modified:   ldm_custom_pipeline/src/metrics/PSNR.py

--- ldm_custom_pipeline/src/constants/constants.py
+++ ldm_custom_pipeline/src/constants/constants.py
@@ -1,2 +1,3 @@
-VAE_COEF = 0.18215
-ETA = 1
+VAE_COEF = 0.18215  # from paper
+ETA = 1  # 0 default
+PSNR_MAX_PIXEL = 1  # Since the images are between -1 and 1
--- ldm_custom_pipeline/src/metrics/PSNR.py
+++ ldm_custom_pipeline/src/metrics/PSNR.py
@@ -1,12 +1,14 @@
 import torch
 
+from src.constants.constants import PSNR_MAX_PIXEL
+
 
 def PSNR(target, prediction):
     target =  target * 2 - 1 # mapped to (-1, 1) 
     mse = torch.mean((target - prediction) ** 2)
     if mse == 0:
         return float('inf')
-    max_pixel = 1.0  # Since the images are between -1 and 1
+    max_pixel = PSNR_MAX_PIXEL
     psnr = 20 * torch.log10(max_pixel / torch.sqrt(mse))
     return psnr.item()
 
